0b93e72904ecb20e5a6b5bcdae68d63872bffef2
Mehdi Rtal <70097433+MehdiRtal@users.noreply.github.com>
2022-04-17 10:53:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("package", nargs="+")
-parser.add_argument("-d", "--download", action="store_true")
-parser.add_argument("-q", "--quiet", action="store_false")
+parser.add_argument("package", nargs="*", help="Download and Install a package")
+parser.add_argument("-d", "--download", action="store_true", help="Install a package only")
+parser.add_argument("-q", "--quiet", action="store_false", help="Execute silently")
+parser.add_argument("-l", "--list", action="store_true", help="List Available Packages")
 args = parser.parse_args()
 
 if not os.path.exists("packages.json"):
@@ -63,14 +64,16 @@ def file_path(arg):
   return folder_name + file_name(arg)
 
 def deploy():
-  for package in data if "".join(args.package) == "all" else data and args.package:
+  for package in data if "".join(args.package) == "all" or args.list else data and args.package:
     try:
       if not os.path.exists(folder_name):
          os.makedirs(folder_name)
       if "version" not in data:
         data[package]["version"] = version(package)
         json.dump(data, open("packages.json", "w"), indent = 2)
-      if args.download:
+      if args.list:
+        print(package + " v" + version(package))
+      elif args.download:
         if data[package]["version"] != version(package) and os.path.exists(file_path(package)):
           os.remove(file_path(package))
         if not os.path.exists(file_path(package)):
